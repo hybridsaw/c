f5ce0af5bcd674c26c51cce18be984e046f14614
Hiromu Teraguchi <ld-0812@hotmail.co.jp>
2014-05-17 12:18:33 +0900

    c
    c

---
+++ test.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("<!DOCTYPE html>\n");
+	printf("<meta charaset=\"UTF-8\">\n");
+	printf("<title>99</title>\n");
+	printf("<hl>99</hl>\n");
+	printf("<table>\n");
+	printf("<tr>\n");
+	printf("<th>\n");
+	printf("<tr>");
+	for (int y = 0; y<10; ++y){
+		for (int x = 0; x<10; ++x){
+			if (x == 0 || y == 0){
+				printf("<th>%d",x+y);
+			}
+			else if (x == 0 && y == 0){
+				printf("<th>");
+			}
+			else{
+				printf("<th>%d", x*y);
+			}
+			if (x == 9){
+				printf("\n");
+			}
+		}
+	}
+	return 0;
+}
